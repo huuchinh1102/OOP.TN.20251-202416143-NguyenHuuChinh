2ae1ec30741349e4ccce71ca14a393485c1dc927
huuchinh1102 <chinhnguyenhuu1102@gmail.com>
2025-11-18 23:21:57 +0700

    Inheritance hierarchy

--- AimsProject/src/com/hust/kstn/models/Book.java
+++ AimsProject/src/com/hust/kstn/models/Book.java
@@ -3,44 +3,19 @@ package com.hust.kstn.models;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book {
-    private static int nbBooks = 0;
-    private int id;
-    private String title;
-    private String category;
-    private float cost;
+public class Book extends Media {
     private int numOfTokens;
     private List<BookAuthor> authors = new ArrayList<>();
 
     public Book(String title, String category, float cost, int numOfTokens) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
+        super(title, category, cost);
         this.numOfTokens = numOfTokens;
-        nbBooks++;
-        this.id = nbBooks;
     }
 
     public void addAuthor(BookAuthor author) {
         if (!authors.contains(author)) {
             authors.add(author);
         }
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public float getCost() {
-        return cost;
     }
 
     public int getNumOfTokens() {
@@ -53,11 +28,6 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book[" + this.id + "]["
-                + this.title + "]["
-                + this.cost + "]["
-                + this.category + "]["
-                + this.numOfTokens + "]"
-                + "\nAuthors: " + this.authors;
+        return super.toString() + "\nTokens: " + this.numOfTokens + "\nAuthors: " + this.authors;
     }
 }
--- AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
@@ -1,68 +1,21 @@
 package com.hust.kstn.models;
 
-public class DigitalVideoDisc {
-    private static int nbDigitalVideoDiscs = 0;
-    private int id;
-    private String title;
-    private String category;
-    private String director;
-    private int length;
-    private double cost;
-
-    public int getId() {
-        return id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getCategory() {
-        return category;
-    }
-    public String getDirector() {
-        return director;
-    }
-    public int getLength() {
-        return length;
-    }
-    public double getCost() {
-        return cost;
-    }
-
-    public DigitalVideoDisc(String title) {
-        this.title = title;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+public class DigitalVideoDisc extends Disc {
+    
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super(title, category, director, length, cost);
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+        super(title, category, cost);
     }
 
-    public DigitalVideoDisc(String title, String category, String director, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+    public DigitalVideoDisc(String title) {
+        super(title, null, 0);
     }
 
     @Override
     public String toString() {
-        return "DVD[" + this.id + "] - [" + this.title + "] - [" + this.category + "] - [" + this.director + "] - [" + this.length + "]: " + this.cost + " $";
+        return "DVD - " + super.getTitle() + " - " + super.getCategory() + " - " + super.getDirector() + " - " + super.getLength() + ": " + super.getCost() + " $";
     }
 }
